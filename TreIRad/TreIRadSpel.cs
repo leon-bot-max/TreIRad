dd2160e3071b78082c10650eef0d9d1ce47ee4ca
L S <leon9807box02@outlook.com>
2020-04-24 12:02:21 +0200

    görDrag()

--- TreIRad/TreIRadSpel.cs
+++ TreIRad/TreIRadSpel.cs
@@ -10,11 +10,19 @@ namespace TreIRad
     {
 
         char[] bräda;
+        char tur = 'X';
+        char väntandeSpelare = 'O';
         //bool eller char vems tur det är
 
-        public TreIRadSpel()
+        public TreIRadSpel(int storlek)
         {
-            bräda = new char[9];  //Man kan ha en array som ser ut ['','','','','' osv] eller [['','',''], ['','',''] osv]
+            bräda = new char[storlek*storlek];  //Man kan ha en array som ser ut ['','','','','' osv] eller [['','',''], ['','',''] osv]
+        }
+
+
+        public void görDrag(int platta)
+        {
+            bräda[platta] = tur;
         }
 
     }
