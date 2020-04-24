b6771cf0990a6e8db99711c2aa02f34816a280df
L S <leon9807box02@outlook.com>
2020-04-24 12:28:50 +0200

    funktioner TreIRadSpel

--- TreIRad/TreIRadSpel.cs
+++ TreIRad/TreIRadSpel.cs
@@ -9,20 +9,48 @@ namespace TreIRad
     class TreIRadSpel
     {
 
-        char[] bräda;
+        char[,] bräda;
+        int storlek;
         char tur = 'X';
         char väntandeSpelare = 'O';
         //bool eller char vems tur det är
 
         public TreIRadSpel(int storlek)
         {
-            bräda = new char[storlek*storlek];  //Man kan ha en array som ser ut ['','','','','' osv] eller [['','',''], ['','',''] osv]
+            this.storlek = storlek;
+            bräda = new char[storlek, storlek];  //Man kan ha en array som ser ut ['','','','','' osv] eller [['','',''], ['','',''] osv]
         }
 
 
-        public void görDrag(int platta)
+        public bool ärTom(int x, int y)
         {
-            bräda[platta] = tur;
+            return bräda[y, x] == new char(); //Kanske byta plats på x, y??????
+        }
+
+        public void görDrag(int x, int y)
+        {
+            bräda[y, x] = tur;
+
+            char temp = tur;
+            tur = väntandeSpelare;
+            väntandeSpelare = temp;
+        }
+
+
+        public bool ärOavgjort()
+        {
+            return Array.IndexOf(bräda, new char()) < 0; //Index of returnar -1 om det inte finns tom
+        }
+
+
+        public bool ärVinst()
+        {
+            //Vågrätt
+
+            //Lodrätt
+
+            //Diagonalt
+            return false;
         }
 
     }
