c0fd6a1c421fce41c0814946d3314036362890c8
Wiggo Thulemark <superwiggo@live.se>
2020-05-14 09:27:40 +0200

    stängavknapp mm

--- TreIRad/Spelplan3x3.Designer.cs
+++ TreIRad/Spelplan3x3.Designer.cs
@@ -30,6 +30,87 @@
         {
             this.SuspendLayout();
             // 
+            // button0
+            // 
+            this.button0.Location = new System.Drawing.Point(30, 12);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(170, 170);
+            this.button0.TabIndex = 0;
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(206, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 170);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(382, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 170);
+            this.button2.TabIndex = 4;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(30, 188);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(170, 170);
+            this.button3.TabIndex = 5;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(206, 188);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(170, 170);
+            this.button4.TabIndex = 6;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(382, 188);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(170, 170);
+            this.button5.TabIndex = 7;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(30, 364);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(170, 170);
+            this.button6.TabIndex = 8;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(206, 364);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(170, 170);
+            this.button7.TabIndex = 9;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(382, 364);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(170, 170);
+            this.button8.TabIndex = 10;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Button_Click);
+            // 
             // Spelplan3x3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -40,7 +121,6 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Spelplan3x3";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Spelplan3x3";
             this.ResumeLayout(false);
 
         }
